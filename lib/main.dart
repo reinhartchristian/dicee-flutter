d715612c5a3a610d381e7c27aa97179ef5ac5cd4
Reinhart Christian <reinhartchristian17@gmail.com>
2021-06-05 19:30:19 +0700

    dice done

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -6,8 +7,9 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          centerTitle: true,
+          title: Text('HAP-HAP'),
+          backgroundColor: Colors.red[300],
         ),
         body: DicePage(),
       ),
@@ -15,9 +17,48 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceButton = Random().nextInt(6) + 1;
+  int rightDiceButton = Random().nextInt(6) + 1;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                diceChange();
+              },
+              child: Image.asset(
+                'images/dice$leftDiceButton.png',
+              ),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                diceChange();
+              },
+              child: Image.asset(
+                'images/dice$rightDiceButton.png',
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void diceChange() {
+    return setState(() {
+      leftDiceButton = Random().nextInt(6) + 1;
+      rightDiceButton = Random().nextInt(6) + 1;
+    });
   }
 }
